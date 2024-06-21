3988368a4349d040b684aec79507da2533b9b2e3
rmasuki <ryotamasuki@gmail.com>
2024-06-21 15:34:49 +0900

    Construct ityp table and get symmetry operations for noncollinar magnets.

--- get_symops.py
+++ get_symops.py
@@ -86,7 +86,7 @@ def parse_qe_cards(file_path):
                 raise ValueError(f'Invalid line in ATOMIC_SPECIES card: {line}')
         elif current_card == 'ATOMIC_POSITIONS':
             parts = line.split()
-            if len(parts) >= 4:
+            if len(parts) == 4:
                 element = parts[0]
                 position = list(map(float, parts[1:4]))
                 atomic_positions.append({
@@ -99,6 +99,8 @@ def parse_qe_cards(file_path):
                 #     'position': position,
                 #     'magnetic_moment': list(map(float, parts[4:])) if len(parts) > 4 else None
                 # })
+            else:
+                raise ValueError(f'Invalid line in ATOMIC_POSITIONS card: {line}')
         elif current_card == 'K_POINTS':
             if 'points' not in k_points:
                 k_points['points'] = []
@@ -110,68 +112,129 @@ def parse_qe_cards(file_path):
         'k_points': k_points
     }
 
+def construct_ityp_table(qe_namelists, qe_cards):
 
-def parse_noncolmag_table(filename, qe_ntyp):
+    # check ntyp and nat
+    qe_ntyp = qe_namelists["system"]["ntyp"]
+    if(qe_ntyp != len(qe_cards["atomic_species"])):
+        raise ValueError("ntyp is not consistent with ATOMIC_SPECIES card")
+    qe_nat = qe_namelists["system"]["nat"]
+    if(qe_nat != len(qe_cards["atomic_positions"])): 
+        raise ValueError("nat is not consistent with ATOMIC_POSITIONS card")
+    
+    # construct ityp table
+    ityp_table = np.zeros(qe_nat, dtype=int)
+    for i in range(qe_nat):
+        element = qe_cards["atomic_positions"][i]["element"]
+        for j in range(qe_ntyp):
+            if element == qe_cards["atomic_species"][j][0]:
+                ityp_table[i] = j
+                break
+    return ityp_table
+    
 
+def get_noncol_magmoms(qe_namelists, qe_cards):
 
-    with open(filename, "r") as f:
-        lines = f.readlines()
-
-    magmom_table = np.zeros(qe_ntyp)
+    # get magnetic moments for each atomic species
+    mag_table = np.zeros(qe_ntyp)
     angle1_table = np.zeros(qe_ntyp)
     angle2_table = np.zeros(qe_ntyp)
 
-    pattern_mags = re.compile(rf'starting_magnetization\((\d+)\)\s*=\s*([+-]?[0-9]*\.?[0-9]+)')
-    pattern_angle1 = re.compile(rf'angle1\((\d+)\)\s*=\s*([+-]?[0-9]*\.?[0-9]+)')
-    pattern_angle2 = re.compile(rf'angle2\((\d+)\)\s*=\s*([+-]?[0-9]*\.?[0-9]+)')
+    pattern_mags = re.compile(rf'starting_magnetization\((\d+)\)')
+    pattern_angle1 = re.compile(rf'angle1\((\d+)\)')
+    pattern_angle2 = re.compile(rf'angle2\((\d+)\)')
 
-    for line in lines:
-        match = pattern_mags.match(line.strip())
+    for key, val in qe_namelists["system"].items():
+        match = pattern_mags.match(key)
         if match:
             index = int(match.group(1))
-            value = float(match.group(2))
-            magmom_table[index-1] = value
-        match = pattern_angle1.match(line.strip())
+            mag_table[index-1] = float(val)
+        match = pattern_angle1.match(key)
         if match:
             index = int(match.group(1))
-            value = float(match.group(2))
-            angle1_table[index-1] = value
-        match = pattern_angle2.match(line.strip())
+            angle1_table[index-1] = float(val)
+        match = pattern_angle2.match(key)
         if match:
             index = int(match.group(1))
-            value = float(match.group(2))
-            angle2_table[index-1] = value
-    
-    return magmom_table, angle1_table, angle2_table
+            angle2_table[index-1] = float(val)
+
+    # construct magmom_table
+    magmom_table = np.zeros([qe_nat, 3])
+    for i in range(qe_ntyp):
+        mag = mag_table[i]
+        theta = angle1_table[i]/180.0*np.pi
+        phi = angle2_table[i]/180.0*np.pi
+        magmom_table[i,0] = mag*np.sin(theta)*np.cos(phi)
+        magmom_table[i,1] = mag*np.sin(theta)*np.sin(phi)
+        magmom_table[i,2] = mag*np.cos(theta)
+
+    # construct ityp table
+    itype_table = construct_ityp_table(qe_namelists, qe_cards)
+
+    # get magnetic moments for each atom
+    magmoms = np.zeros([qe_nat, 3])
+    for i in range(qe_nat):
+        magmoms[i] = magmom_table[itype_table[i]]
+
+    return magmoms
 
 # parse input file of quantum espresso
 filename = "pw.Mn3Sn.scf.in"
 qe_namelists = parse_namelist(filename, ["control", "system", "electrons"])
 qe_cards = parse_qe_cards(filename)
 
-print(qe_namelists["system"].keys())
-print(qe_cards["atomic_species"])
-print(qe_cards["atomic_positions"])
-print(qe_cards["k_points"])
+# print(qe_namelists["system"].keys())
+# print(qe_cards["atomic_species"])
+# print(qe_cards["atomic_positions"])
+# print(qe_cards["k_points"])
 
 
 qe_ntyp = qe_namelists["system"]["ntyp"]
 qe_nat = qe_namelists["system"]["nat"]
-print("ntype = ", qe_ntyp)
-print("nat = ", qe_nat)
+# print("ntype = ", qe_ntyp)
+# print("nat = ", qe_nat)
 
 # read the structure from the espresso input using ase.io
 atoms = read(filename, format="espresso-in")
 
-print(atoms.get_cell())
-print(atoms.get_scaled_positions())
-print(atoms.get_atomic_numbers())
-print(atoms.get_initial_magnetic_moments())
+# print(atoms.get_cell())
+# print(atoms.get_scaled_positions())
+# print(atoms.get_atomic_numbers())
+# print(atoms.get_initial_magnetic_moments())
 # print(atoms.get_magnetic_moments())
 
-# magnetization
-magmom_table, angle1_table, angle2_table = parse_noncolmag_table(filename, qe_ntyp)
-print(magmom_table)
-print(angle1_table)
-print(angle2_table)
+
+# get symmetry operations
+lattice = atoms.get_cell()
+positions = atoms.get_scaled_positions()
+numbers = atoms.get_atomic_numbers()
+magmoms = get_noncol_magmoms(qe_namelists, qe_cards)
+cell = (lattice, positions, numbers, magmoms)
+
+Amat = np.array(lattice)
+Ainv = np.linalg.inv(Amat)
+
+dataset_as_noncollinear = get_magnetic_symmetry_dataset(cell, symprec=1e-2, is_axial=True)
+
+uni_number = dataset_as_noncollinear["uni_number"]  # Range from 1 to 1651
+magnetic_spacegroup_type = get_magnetic_spacegroup_type(uni_number)
+nsym = len(dataset_as_noncollinear["rotations"])
+
+# print space group information
+# print("magnetic spacegroup type : ", magnetic_spacegroup_type)
+print("number of symmetry operations : ", nsym)
+for isym in range(nsym):
+    print("Symmetry operation : ", isym)
+    print("Rotation :")
+    print(dataset_as_noncollinear["rotations"][isym])
+    print("Rotation in Cartesian coordinates :")
+    print(np.dot(Amat.transpose(), 
+          np.dot(dataset_as_noncollinear["rotations"][isym], 
+          Ainv.transpose())))
+    
+    print("Translation :")
+    print(dataset_as_noncollinear["translations"][isym])
+
+    print("Time reversal :", dataset_as_noncollinear["time_reversals"][isym])
+
 
